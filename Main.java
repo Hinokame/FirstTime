bfc3327afc4af8ac7aec2d828d1adeee6aee1440
Andrius Alimas <andriusjavait@gmail.com>
2015-07-08 03:48:53 +0100

    comment

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ package com.andrew;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code herehgyhghgjhbhgjhghjgsdss
+	// write your code herehgyhghgjhbhgjhghjgsdsslll
     }
 }
